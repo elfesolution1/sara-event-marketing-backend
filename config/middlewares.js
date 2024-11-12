7abea213074b385ed7953d2d5d3d20a6a2deead7
elfesolution1 <elfesolution1@gmail.com>
2024-11-12 15:00:53 +0300

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://sara-event-and-marketing-frontend-2lsrfp7if.vercel.app'], // Add your frontend domain(s)
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
